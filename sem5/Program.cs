047f0851618c3669158ae931fd9d8d63bb0e2d12
Konstantin Smirnov <sasharugby@yandex.ru>
2024-08-07 20:08:20 +0300

    122done

--- sem5/Program.cs
+++ sem5/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public bool IsRead { get; set; }
+}
+
+
+public class MessageContext : DbContext
+{
+    public DbSet<Message> Messages { get; set; }
+}
+
+
+public List<Message> GetUnreadMessages(int clientId)
+{
+    using (var context = new MessageContext())
+    {
+        List<Message> unreadMessages = context.Messages.Where(m => !m.IsRead && m.ClientId == clientId).ToList();
+        return unreadMessages;
+    }
+}
+
+
+List<Message> unreadMessages = GetUnreadMessages(1);
+
+/
+foreach (var message in unreadMessages)
+{
+    Console.WriteLine(message.Text);
+}
